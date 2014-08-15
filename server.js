d9760ce0b7ecd34ae2d8ba79c42420d26ef20cea
Linan Qiu <linanqiu@gmail.com>
2014-08-15 20:52:54 +0800

    json for schedule

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
+// you know what these are
 var express = require('express');
 var fs = require('fs');
 var cheerio = require('cheerio');
 var bodyParser = require('body-parser');
 var https = require('https');
 var tough = require('tough-cookie');
+
 https.globalAgent.options.secureProtocol = 'SSLv3_method';
 
+// let's set up our app to throw jsons around
 var app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+// roar lion roar
 var host = 'https://ssol.columbia.edu';
 
+// template cookie jar. SSOL has a screwed up cookie. key test value on. you kidding me?
 var jarTemplate = {
 	"_jar": {
 		"store": {
@@ -35,7 +40,7 @@ var jarTemplate = {
 	}
 };
 
-// gets the loginToken
+// gets the sessionToken from username and password
 app.get('/auth/login', function(req, res) {
 	var request = require('request');
 	var j = request.jar();
@@ -44,7 +49,6 @@ app.get('/auth/login', function(req, res) {
 		if (err) {
 			throw err;
 		}
-		$ = cheerio.load(body);
 
 		var creation;
 		var sessionToken;
@@ -124,11 +128,66 @@ app.get('/academic/schedule', function(req, res) {
 		if (err) {
 			throw err;
 		}
-		res.status(200).send(body);
+		var message = formatSchedule(body);
+		res.status(200).send(message);
 	});
 });
 
-var port = Number(process.env.PORT || 5000);
+function formatSchedule(body) {
+	$ = cheerio.load(body);
 
+	var classes = [];
+	var rows = $('#Content > table:nth-child(4) tr').each(function(i, elem) {
+		if($(this).find('[bgcolor="#eeeeee"]').length > 0) {
+
+			try {
+				var scheduleClass = {};
+
+				$(this).find('td').each(function(j, element) {
+
+					var elementTexts = $(element).html().split('&#xA0;');
+
+					switch(j) {
+						case 0: 
+						scheduleClass['dept'] = elementTexts[0];
+						scheduleClass['courseCode'] = elementTexts[1];
+						scheduleClass['section'] = elementTexts[3].substring(0,3);
+						scheduleClass['fullName'] = $(element).find('font').text();
+						break;
+						case 1: scheduleClass['grading'] = elementTexts[1].replace(/[^0-9\.]/g, '');
+						break;
+						case 2: 
+						scheduleClass['instructorName'] = elementTexts[0] + " " + elementTexts[1].split('<br>')[0];
+						scheduleClass['instructorEmail'] = $(element).find('a').text();
+						break;
+						case 3: scheduleClass['day'] = elementTexts[0].trim().split(' ');
+						break;
+						case 4: 
+						scheduleClass['time'] = elementTexts[0].split('<br>')[0];
+						scheduleClass['room'] = elementTexts[0].split('<br>')[1];
+						scheduleClass['building'] = elementTexts[1];
+						break;
+						case 5: 
+						scheduleClass['startDate'] = elementTexts[0].split('<br>')[0];
+						scheduleClass['endDate'] = elementTexts[0].split('<br>')[1];
+						break;
+						default: 
+						break;
+					}
+				});
+
+				classes.push(scheduleClass);
+			} catch (err) {
+				console.log("error");
+			}
+		}
+	});
+
+	return classes;
+}
+
+// initialize the server
+var port = Number(process.env.PORT || 5000);
+console.log("Server running on port " + port);
 app.listen(port);
 exports = module.exports = app;
