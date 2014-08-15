007934df0be86ad62f418f338af8835e7ae9f259
Linan Qiu <linanqiu@gmail.com>
2014-08-15 17:17:34 +0800

    proper auth

--- server.js
+++ server.js
@@ -35,71 +35,68 @@ var jarTemplate = {
 	}
 };
 
-// gets the URL to post for login
-app.get('/auth/getLoginUrl', function(req, res) {
+// gets the loginToken
+app.get('/auth/login', function(req, res) {
 	var request = require('request');
 	var j = request.jar();
 
-	request({url: host, jar: j}, function(err, resp, body) {
+	var getSessionToken = function(err, resp, body) {
 		if (err) {
 			throw err;
 		}
 		$ = cheerio.load(body);
 
 		var creation;
-		var loginUrl;
+		var sessionToken;
 
 		for(path in j['_jar']['store']['idx']['ssol.columbia.edu']) {
 			creation = j['_jar']['store']['idx']['ssol.columbia.edu'][path]['test']['creation'];
-			loginUrl = path.replace('\/cgi-bin\/ssol\/', '');
+			sessionToken = path.replace('\/cgi-bin\/ssol\/', '');
 		}
 
-		res.status(200).send({
-			loginUrl: loginUrl,
+		sessionObject = {
+			sessionToken: sessionToken,
 			creation: creation,
-		});
-	});
-});
+		};
 
-// gets the loginToken
-app.get('/auth/login', function(req, res) {
-	var request = require('request');
-	var j = request.jar();
+		var cookieTemp = JSON.parse(JSON.stringify(jarTemplate['_jar']['store']['idx']));
+		var pathTemp = '/cgi-bin/ssol/' + sessionObject['sessionToken'];
+		cookieTemp['ssol.columbia.edu'][pathTemp] = cookieTemp['ssol.columbia.edu']['/cgi-bin/ssol/LOGIN_URL'];
+		delete cookieTemp['ssol.columbia.edu']['/cgi-bin/ssol/LOGIN_URL'];
+		cookieTemp['ssol.columbia.edu'][pathTemp]['test']['path'] = pathTemp;
+		cookieTemp['ssol.columbia.edu'][pathTemp]['test']['creation'] = sessionObject['creation'];
+		cookieTemp['ssol.columbia.edu'][pathTemp]['test']['lastAccessed'] = sessionObject['creation'];
 
-	var cookieTemp = JSON.parse(JSON.stringify(jarTemplate['_jar']['store']['idx']));
-	var pathTemp = '/cgi-bin/ssol/' + req.param('loginUrl');
-	cookieTemp['ssol.columbia.edu'][pathTemp] = cookieTemp['ssol.columbia.edu']['/cgi-bin/ssol/LOGIN_URL'];
-	delete cookieTemp['ssol.columbia.edu']['/cgi-bin/ssol/LOGIN_URL'];
-	cookieTemp['ssol.columbia.edu'][pathTemp]['test']['path'] = pathTemp;
-	cookieTemp['ssol.columbia.edu'][pathTemp]['test']['creation'] = req.param('creation');
-	cookieTemp['ssol.columbia.edu'][pathTemp]['test']['lastAccessed'] = req.param('creation');
-
-	j = {
-		"_jar": {
-			"store": {
-				"idx": cookieTemp
+		j = {
+			"_jar": {
+				"store": {
+					"idx": cookieTemp
+				}
 			}
-		}
-	};
+		};
 
-	var formData = {
-		p_r_id: req.param('loginUrl'),
-		p_t_id: 1,
-		jsen: 'Y',
-		'tran[1]_tran_name': "slin",
-		u_id: req.param('username'),
-		u_pw: req.param('password'),
-		submit: "Continue",
-		reset: "Clear"
-	};
+		var formData = {
+			p_r_id: sessionObject['sessionToken'],
+			p_t_id: 1,
+			jsen: 'Y',
+			'tran[1]_tran_name': "slin",
+			u_id: req.param('username'),
+			u_pw: req.param('password'),
+			submit: "Continue",
+			reset: "Clear"
+		};
 
-	request.post(host + "/cgi-bin/ssol/" + req.param('loginUrl'), {form: formData}, function(err, resp, body) {
-		if (err) {
-			throw err;
-		}
-		res.send(body);
-		console.log(JSON.stringify(j, null, 4));
-	});
+		request.post(host + "/cgi-bin/ssol/" + sessionObject['sessionToken'], {form: formData}, function(err, resp, body) {
+			if (err) {
+				throw err;
+			}
+			res.status(200).send({
+				sessionToken: sessionObject['sessionToken']
+			});
+		});
+	}
+
+	request({url: host, jar: j}, getSessionToken);
 });
 
 // gets schedule
@@ -108,7 +105,7 @@ app.get('/academic/schedule', function(req, res) {
 	var j = request.jar();
 
 	var cookieTemp = JSON.parse(JSON.stringify(jarTemplate['_jar']['store']['idx']));
-	var pathTemp = '/cgi-bin/ssol/' + req.param('loginUrl');
+	var pathTemp = '/cgi-bin/ssol/' + req.param('sessionToken');
 	cookieTemp['ssol.columbia.edu'][pathTemp] = cookieTemp['ssol.columbia.edu']['/cgi-bin/ssol/LOGIN_URL'];
 	delete cookieTemp['ssol.columbia.edu']['/cgi-bin/ssol/LOGIN_URL'];
 	cookieTemp['ssol.columbia.edu'][pathTemp]['test']['path'] = pathTemp;
@@ -123,11 +120,11 @@ app.get('/academic/schedule', function(req, res) {
 		}
 	};
 
-	request({url: host + "/cgi-bin/ssol/" + req.param('loginUrl') + '?p%.5Fr%.5Fid=RUBBISH&p%.5Ft%.5Fid=1&tran%.5B1%.5D%.5Fentry=student&tran%.5B1%.5D%.5Ftran%.5Fname=ssch', jar: j}, function(err, resp, body) {
+	request({url: host + "/cgi-bin/ssol/" + req.param('sessionToken') + '?p%.5Fr%.5Fid=RUBBISH&p%.5Ft%.5Fid=1&tran%.5B1%.5D%.5Fentry=student&tran%.5B1%.5D%.5Ftran%.5Fname=ssch', jar: j}, function(err, resp, body) {
 		if (err) {
 			throw err;
 		}
-		res.send(body);
+		res.status(200).send(body);
 	});
 });
 
